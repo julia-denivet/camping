cdbbbde3ce178233900233367f2b34ac026af22a
luc van der meijden <luc.van-der-meijden@laplateforme.io>
2020-01-27 14:26:25 +0100

    Page Accueil - Connexion - Inscription - Profil - Contact terminé

--- inscription.php
+++ inscription.php
@@ -25,18 +25,13 @@
 			<section id="inscription_form">
 				<div id="inscription_form_opacite">
 					<form method="post" enctype="multipart/form-data">
-						<div class="inscription_form_partie">
-							<input type="text" name="login" class="inscription_2" placeholder="LOGIN"/>
-							<input type="text" name="prenom" class="inscription_2" placeholder="PRENOM"/>
-							<input type="password" name="passe" class="inscription_2" placeholder="MOT DE PASSE"/>
-						</div>
-						<div class="inscription_form_partie">
-							<input type="mail" name="email" class="inscription_2" placeholder="E-MAIL"/>
-							<input type="text" name="nom" class="inscription_2" placeholder="NOM"/>
-							<input type="password" name="passe2" class="inscription_2" placeholder="CONFIRMATION MOT DE PASSE"/>
-						</div>
+						<input type="text" name="login" class="inscription_2" placeholder="LOGIN"/>
+						<input type="mail" name="email" class="inscription_2" placeholder="E-MAIL"/>
+						<input type="text" name="prenom" class="inscription_2" placeholder="PRENOM"/>
+						<input type="text" name="nom" class="inscription_2" placeholder="NOM"/>
+						<input type="password" name="passe" class="inscription_2" placeholder="MOT DE PASSE"/>
+						<input type="password" name="passe2" class="inscription_2" placeholder="CONFIRMATION MOT DE PASSE"/>
 						<div class="inscription_2">
-							<label for="inscription_img">Choisissez une image de profil : </label>
 							<input type="file" id="inscription_img" name="image" accept="image/png, image/jpeg, image/jpg">
 						</div>
 						<input type="submit" value="INSCRIPTION" name="inscription" class="inscription_2"/>
