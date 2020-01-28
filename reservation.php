49e09cb8c36018d91dd2b8a726bf5293f0c5da65
julia-denivet <julia.denivet@laplateforme.io>
2020-01-28 18:27:34 +0100

    Commit

--- reservation.php
+++ reservation.php
@@ -24,8 +24,70 @@
 		<main id="reservation">
 			
 			<?php
-				include("verification/verif-reservation.php");
+				if (isset($_SESSION['login'])) 
+				{
+					$connexion = mysqli_connect("localhost", "root","", "camping");
+					$requete = "SELECT * FROM reservation WHERE id_utilisateur='".$_SESSION['id']."'";
+					$resultat = mysqli_query($connexion, $requete);
+					
+					
+				}  else header('location: connexion.php');	
 			?>
+				<section>
+					<article>
+							<div>Arrivée</div>
+							<div>Départ</div>
+							<div>Lieu</div>
+							<div>Options</div>
+					</article>
+				</section>
+			<?php
+					while ($donnees = mysqli_fetch_array($resultat))
+					
+					
+					{
+   
+							   $requete = "SELECT login FROM utilisateurs WHERE id = '".$donnees['id_utilisateur']."'";
+							   $req = mysqli_query($connexion, $requete);
+							   $data = mysqli_fetch_assoc($req);
+							
+						   
+							   echo"<article>".$donnees['3']." ".$donnees['4']."  ".$donnees['lieu']."";
+									
+								   if ($donnees['5']== 'on') 
+								   {
+									  echo 'borne électrique';
+								   }
+								   if ($donnees['disco']== 'on') 
+								   {
+									  echo 'Disco-Club “Les girelles dansantes”';
+								   }
+								   if ($donnees['activites']== 'on') 
+								   {
+									  echo 'activités Yoga, Frisbee et Ski Nautique';
+								   }
+
+								echo "</article>";
+								    
+							   
+							 
+							   
+								   
+					} 
+		
+						   mysqli_close($connexion); 
+					
+					
+
+				
+
+			
+				
+			?>	
+				
+
+
+			
 		</main>
 		
 		<footer>
