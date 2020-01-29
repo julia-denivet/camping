abcce499a8d0f6bad8a8a669770dcdf16a265fb7
luc van der meijden <luc.van-der-meijden@laplateforme.io>
2020-01-29 17:22:18 +0100

    MODIF

--- reservation.php
+++ reservation.php
@@ -22,10 +22,90 @@
 		</header>
 		
 		<main id="reservation">
+			<section id="reservation_form">
+				<section id="reservation_affichage_header">
+					<div>ARRIVEE</div>
+					<div>DEPART</div>
+					<div>LIEU</div>
+					<div>OPTION</div>
+					<div>PRIX</div>
+				</section>
+				
+				<?php
+					$connexion = mysqli_connect("localhost", "root", "", "camping");
+					$requete = "SELECT * FROM reservation WHERE id_utilisateurs = '".$_SESSION['id']."'";
+					$resultat = mysqli_query($connexion, $requete);
+
+					while($donnees = mysqli_fetch_array($resultat))
+					{
+						echo '<section id="reservation_affichage_header">';
+							echo '<div>', $donnees['arrive'], '</div>';
+							echo '<div>', $donnees['depart'], '</div>';
+							echo '<div>', $donnees['lieu'], '</div>';
+							echo '<div>';
+								if($donnees['electrique'] == 'on'){echo 'Borne électrique';}
+								if($donnees['disco'] == 'on'){echo 'Club Disco';}
+								if($donnees['activites'] == 'on'){echo 'Activités';}
+							echo '</div>';
+							echo '<div>', $donnees['prix'], '€</div>';
+						echo '</section>';
+					}
+				?>
+			</section>
 			
-			<?php
-				include("verification/verif-reservation.php");
-			?>
+			<section id="reservation_form">
+				<h1>NOUVELLE RESERVATION</h1>
+				<form method="post">
+					<div class="double_fieldset">
+						<fieldset><legend>EMPLACEMENT</legend>
+							<select name="emplacement" class="reservation_form_css">
+								<option value="">--Type d'emplacement--</option>
+								<option value="tente">Tente</option>
+								<option value="campingcar">Camping car</option>
+							</select>
+						</fieldset>
+						<fieldset><legend>LIEUX</legend>
+							<select name="lieux" class="reservation_form_css">
+								<option value="">--Lieux--</option>
+								<option value="plage">La Plage</option>
+								<option value="pins">Les Pins</option>
+								<option value="marquis">Le Maquis</option>
+							</select>
+						</fieldset>
+					</div>
+					
+					<div class="double_fieldset">
+						<fieldset><legend>ARRIVEE</legend>
+							<input type="date" name="debut" class="reservation_form_css" value="<?php echo date('Y-m-d'); ?>"/>
+						</fieldset>
+						<fieldset><legend>DEPART</legend>
+							<input type="date" name="fin" class="reservation_form_css" value="<?php echo date('Y-m-d'); ?>"/>
+						</fieldset>
+					</div>
+
+					<fieldset><legend>OPTIONS</legend>
+						<div id="option">
+							<label for="electrique">
+								<input type="checkbox" name="electrique"/>
+								Borne électrique +2€
+							</label>
+							<label for="disco">
+								<input type="checkbox" name="disco"/>
+								Disco-Club +17€
+							</label>
+							<label for="activites">
+								<input type="checkbox" name="activites"/>
+								Activités Yoga, Frisbee et Ski Nautique +30€
+							</label>
+						</div>
+					</fieldset>
+					
+					<input type="submit" id="reservation_form_validation" value="RESERVER" name="reservation"/>
+					<?php
+						include("verification/verif-reservation.php");
+					?>
+				</form>
+			</section>
 		</main>
 		
 		<footer>
--- verification/verif-reservation.php
+++ verification/verif-reservation.php
@@ -1,3 +1,23 @@
 <?php
-	
+	if(isset($_POST['reservation']))
+	{
+		if(!empty($_POST['emplacement']) && !empty($_POST['lieux']) && !empty($_POST['debut']) && !empty($_POST['fin']) && !empty($_POST['electrique']) && !empty($_POST['disco']) && !empty($_POST['activites']))
+		{
+			$emplacement = $_POST['emplacement'];
+			$lieux = $_POST['lieux'];
+			$debut = $_POST['debut'];
+			$fin = $_POST['fin'];
+			$electrique = $_POST['electrique'];
+			$disco = $_POST['disco'];
+			$activites = $_POST['activites'];
+			$id_utilisateurs = $_SESSION['id'];
+			
+			$sql= "INSERT INTO reservation (lieu, type, arrive, depart, electrique, disco, activites, id_utilisateurs) VALUES ('$lieux', '$emplacement', '$debut', '$fin', '$electrique', '$disco', '$activites', '$id_utilisateurs')";
+			mysqli_query($connexion, $sql);
+			mysqli_close($connexion);
+		}
+		else{
+			echo "Veuillez remplir toutes les casses";
+		}
+	}
 ?>
